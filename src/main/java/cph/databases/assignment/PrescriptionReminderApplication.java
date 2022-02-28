36110c76689e6f0bb64f07a2d4512b65237bd252
kasarama <cph-mw216@cphbusiness.dk>
2022-03-01 00:07:16 +0100

    init commit

---
+++ src/main/java/cph/databases/assignment/PrescriptionReminderApplication.java
@@ -0,0 +1,15 @@
+package cph.databases.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrescriptionReminderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrescriptionReminderApplication.class, args);
+
+    }
+
+
+}
